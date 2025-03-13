7e72f7d3c984f8ef471114965d2d40166f218668
Awantika <awantikakumari2003@gmail.com>
2025-03-13 15:29:09 +0530

    updat

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,23 +7,24 @@
 
     <!-- Desktop Navigation -->
     <ul class="hidden md:flex space-x-6">
-        <li><a href="#" class="text-orange-500">Home</a></li>
+        <li><a href="/" class="text-orange-500">Home</a></li>
 
         <!-- Dropdown Parent -->
         <li class="relative group">
-            <a href="#" class="hover:text-orange-500">Programme ▼</a>
+            <a href="" class="hover:text-orange-500">Programme ▼</a>
             <!-- Dropdown Menu -->
             <ul class="absolute left-0 hidden group-hover:block  bg-white text-black shadow-lg rounded-md w-40">
+                <li><a href="/media" class="block px-4 py-2 hover:bg-orange-400">Media</a></li>
                 <li><a href="#" class="block px-4 py-2 hover:bg-orange-400">Agenda</a></li>
                 <li><a href="#" class="block px-4 py-2 hover:bg-orange-400">Speakers</a></li>
                 <li><a href="#" class="block px-4 py-2 hover:bg-orange-400">Sessions</a></li>
             </ul>
         </li>
 
-        <li><a href="#" class="hover:text-orange-500">Overview</a></li>
-        <li><a href="#" class="hover:text-orange-500">Blog</a></li>
-        <li><a href="#" class="hover:text-orange-500">Event</a></li>
-        <li><a href="#" class="hover:text-orange-500">Contact Us</a></li>
+        <li><a href="/about" class="hover:text-orange-500">Overview</a></li>
+        <li><a href="/blog" class="hover:text-orange-500">Blog</a></li>
+        <li><a href="/event" class="hover:text-orange-500">Event</a></li>
+        <li><a href="/contact" class="hover:text-orange-500">Contact Us</a></li>
     </ul>
 
     <!-- Buttons -->
@@ -45,7 +46,7 @@
     <div id="mobile-menu" class="fixed top-0 left-0 w-full bg-white shadow-lg p-6 hidden flex-col z-50 transition-transform transform -translate-y-full">
         <button id="close-menu" class="text-3xl absolute top-4 right-6">&times;</button>
         <ul class="flex flex-col space-y-6 text-center mt-12">
-            <li><a href="#" class="text-orange-500">Home</a></li>
+            <li><a href="/" class="text-orange-500">Home</a></li>
             
             <!-- Mobile Dropdown -->
             <li class="relative">
@@ -53,16 +54,17 @@
                     Programme ▼
                 </button>
                 <ul id="mobile-dropdown-menu" class="hidden mt-2 bg-white text-black shadow-lg rounded-md w-full">
+                    <li><a href="/media" class="block px-4 py-2 hover:bg-orange-400">Media</a></li>
                     <li><a href="#" class="block px-4 py-2 hover:bg-orange-400">Agenda</a></li>
-                <li><a href="#" class="block px-4 py-2 hover:bg-orange-400">Speakers</a></li>
-                <li><a href="#" class="block px-4 py-2 hover:bg-orange-400">Sessions</a></li>
+                    <li><a href="#" class="block px-4 py-2 hover:bg-orange-400">Speakers</a></li>
+                   <li><a href="#" class="block px-4 py-2 hover:bg-orange-400">Sessions</a></li>
                 </ul>
             </li>
 
-            <li><a href="#" class="hover:text-orange-500">Overview</a></li>
-            <li><a href="#" class="hover:text-orange-500">Blog</a></li>
-            <li><a href="#" class="hover:text-orange-500">Event</a></li>
-            <li><a href="#" class="hover:text-orange-500">Contact Us</a></li>
+            <li><a href="/about" class="hover:text-orange-500">Overview</a></li>
+            <li><a href="/bog" class="hover:text-orange-500">Blog</a></li>
+            <li><a href="/event" class="hover:text-orange-500">Event</a></li>
+            <li><a href="/contact" class="hover:text-orange-500">Contact Us</a></li>
         </ul>
         <div class="flex space-x-4 justify-center">
             <a href="#" class="bg-[#ff4e02] text-white px-4 py-2 rounded-full border border-white hover:bg-[##3d8a2c] transition-all">
